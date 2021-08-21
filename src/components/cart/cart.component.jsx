4576cd710edf0384144c53bbc1c3f097a292f954
abc <abc@example.com>
2021-08-21 20:40:49 +0430

    created cart-item component

--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-buton.component';
+import CartItem from '../cart-item/cart-item.component';
+import { connect } from 'react-redux';
 import './card.styles.scss';
 
 
-const Cart=()=>(
+const Cart=({cartItems})=>(
     <div className='cart'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
+        {
+            cartItems.map(cartItem=><CartItem key={cartItem.id} item={cartItem}/>)
+        }
+        </div>
         <CustomButton>Go TO CHECKOUT</CustomButton>
     
     </div>
 )
 
-export default Cart;
+
+const mapStateToProps= ({cart:{cartItems}}) =>({
+    cartItems
+})
+
+export default connect(mapStateToProps)(Cart) ;
